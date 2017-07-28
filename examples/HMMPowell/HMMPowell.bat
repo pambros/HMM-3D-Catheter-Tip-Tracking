de01e05ce2f8208a1b77827e3f90cb4012a476c6
pambros <pambros@users.noreply.github.com>
2017-07-28 17:48:19 +0200

    standardization

--- examples/HMMPowell/HMMPowell.bat
+++ examples/HMMPowell/HMMPowell.bat
@@ -1,6 +1,10 @@
 @call ../../localProjectPath.bat
 @call "%_PROJECT_PATH%/globalVariables.bat"
 
+@if not exist generated (
+	@mkdir "generated"
+)
+
 @set ARGUMENTS=Registration "%_PROJECT_PATH%/data/dataset1" "configHMMPowell.txt" "generated" -debugImages
 %_TACE% %ARGUMENTS%
 
